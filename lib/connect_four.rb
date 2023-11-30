a6506e81d62c3fa7911ef060403c488677ae5244
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2023-11-30 23:40:16 +0200

    Used unicode for the column numbers

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -10,12 +10,15 @@ class ConnectFour
   def create_board
     Array.new(6) {Array.new(7)}
   end
+  
+  def update_board(col, color)
+  end
 
   def print_board(array = @board)
     printed = array.map do |row| 
       row.map! { |spot| "\u{2B1C}" if spot.nil? }
     end
-    puts  (1..7).to_a.join(' ')
+    puts  ["\u{24F5}", "\u{24F6}", "\u{24F7}", "\u{24F8}", "\u{24F9}", "\u{24FA}", "\u{24FB}"].join(' ')
     puts printed.map {|line| line.join}
   end
 end
