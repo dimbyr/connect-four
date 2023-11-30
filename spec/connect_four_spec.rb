554ef5299196a61db8b7c957f24edc0c554a3369
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2023-11-30 23:28:03 +0200

    Created a simple test

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,3 +1,20 @@
 # frozen_string_litterals: true
 
-require '../lib/connect_four'
+require './lib/connect_four'
+
+describe ConnectFour do
+  
+  describe "#create_board" do
+    subject(:game) {described_class.new}
+    it 'returns a 6 rows array' do 
+      board = game.board
+      expect(board.length).to eq(6)
+    end
+
+    it 'returns a 7 columns array' do
+      fith_row = game.board[4]
+      expect(fith_row.length).to eq(7)
+    end 
+  end
+
+end
