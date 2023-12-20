4a98b23952127eff0996d266011df5ee08b3da81
Dimby Rabearivony <drabearivony@gmail.com>
2023-12-20 09:51:32 +0200

    Fixed linters

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,5 +1,6 @@
 # frozen_string_litterals: true
 
+# Game board
 class ConnectFour
 
   attr_accessor :board
@@ -11,29 +12,31 @@ class ConnectFour
   def create_board
     Array.new(6) {Array.new(7)}
   end
-  
+
   def update_board(col, color = "\u{2605} ")
     tmp_board = @board
     (0..5).each do |idx|
       current_row = tmp_board[idx]
       next_row = idx < 5 ?  tmp_board[idx + 1] : Array.new(7, 1)
-      tmp_board[idx][col] = color if next_row[col] &&  current_row[col].nil?
+      tmp_board[idx][col] = color if next_row[col] && current_row[col].nil?
     end
     @board = tmp_board
   end
 
   def game_over?
+    false
   end
 
   def print_board
     printed = @board.map do |row| 
       row.map { |spot| spot.nil? ? "\u{2B1C}" : spot }
     end
-    puts  ["\u{24F5}", "\u{24F6}", "\u{24F7}", "\u{24F8}", "\u{24F9}", "\u{24FA}", "\u{24FB}"].join(' ')
-    puts printed.map {|line| line.join}
+    puts ["\u{24F5}", "\u{24F6}", "\u{24F7}", "\u{24F8}", "\u{24F9}", "\u{24FA}", "\u{24FB}"].join(' ')
+    puts printed.map(&:join)
   end
 end
 
+# A player class
 class Player
   attr_accessor :ball
 
@@ -47,19 +50,19 @@ class Player
   end
 
   def getnum
-   puts 'Your turn: 1,...,7'
-   num = gets.chomp.to_i
-   num - 1
+    puts 'Your turn: 1,...,7'
+    num = gets.chomp.to_i
+    num - 1
   end
 end
 
+# A human player
 class Human < Player
 end
-  
-class Computer < Player
 
+# Computer, playing randomly
+class Computer < Player
   def getnum
     rand(0..6)
   end
-
-end 
+end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -3,9 +3,8 @@
 require './lib/connect_four'
 
 describe ConnectFour do
-  
-  describe "#create_board" do
-    subject(:game) {described_class.new}
+  describe '#create_board' do
+    subject(:game) { described_class.new }
     it 'returns a 6 rows array' do 
       board = game.board
       expect(board.length).to eq(6)
@@ -14,7 +13,6 @@ describe ConnectFour do
     it 'returns a 7 columns array' do
       fith_row = game.board[4]
       expect(fith_row.length).to eq(7)
-    end 
+    end
   end
-
 end
